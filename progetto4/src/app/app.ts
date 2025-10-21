e0cbb54e3ff5a31ee67acdc73bbccf2dbbbbdbb9
MATTEO07GIANOTTI <159137363+MATTEO07GIANOTTI@users.noreply.github.com>
2025-10-21 06:42:46 +0000

    fine

--- progetto4/src/app/app.ts
+++ progetto4/src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { Componente1 } from './componente1/componente1';
+import { CommonModule } from '@angular/common';
+import { Post } from './post/post';
 
 @Component({
 selector: 'app-root',
-imports: [Componente1],
+imports: [Componente1,CommonModule,Post],
 templateUrl: './app.html',
 styleUrl: './app.css'
 })
 export class App {
 messaggio = 'Ciao dal componente padre!';
+posts = [
+{
+titolo: 'titolo1',
+testo: 'esempio di testo1'
+},
+{
+titolo: 'titolo1',
+testo: 'esempio di testo2'
+},
+{
+titolo: 'titolo3',
+testo: 'esempio di testo3'
+}
+];
 }
